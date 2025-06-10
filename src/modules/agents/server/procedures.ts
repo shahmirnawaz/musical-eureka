da60dea6a2ff11a363354c576614dc138bd2a330
Shahmir Nawaz Khan <shahmirnawazkhan@gmail.com>
2025-06-10 00:57:31 -0400

    15: agent update delete

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -5,11 +5,50 @@ import { eq, and, ilike, getTableColumns, sql, desc, count } from "drizzle-orm";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agents } from "@/db/schema";
 
-import { agentsInsertSchema } from "../schema";
+import { agentsInsertSchema, agentsUpdateSchema } from "../schema";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
+    update: protectedProcedure
+    .input(agentsUpdateSchema)
+    .mutation(async ({input, ctx}) => {
+        const [updatedAgent] = await db
+        .update(agents)
+        .set(input)
+        .where(
+            and(
+                eq(agents.id, input.id),
+                eq(agents.userId, ctx.auth.user.id),
+            )
+        )
+        .returning();
+
+        if (!updatedAgent) {
+            throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+        }
+
+        return updatedAgent;
+    }),
+    remove: protectedProcedure
+    .input(z.object({id: z.string()}))
+    .mutation(async ({input, ctx}) => {
+        const [removedAgent] = await db
+        .delete(agents)
+        .where(
+            and(
+                eq(agents.id, input.id),
+                eq(agents.userId, ctx.auth.user.id),
+            )
+        )
+        .returning();
+
+        if (!removedAgent) {
+            throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+        }
+
+        return removedAgent;
+    }),
     getOne:protectedProcedure.input(z.object({id: z.string() }))
     .query(async ({input, ctx}) => {
         const [existingAgent] = await db
--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -8,6 +8,12 @@ import { AgentIdViewHeader } from "../components/agent-id-view-header";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useState } from "react";
+import { UpdateAgentDialog } from "../components/update-agent-dialog";
 
 interface Props {
     agentId: string
@@ -15,40 +21,80 @@ interface Props {
 
 export const AgentIdView = ({ agentId }: Props) => {
     const trpc = useTRPC();
-    const { data } = useSuspenseQuery(trpc.agents.getOne.queryOptions({ id: agentId }));
-    return (
-        <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <AgentIdViewHeader
-                agentId={agentId}
-                agentName={data?.name}
-                onEdit={() => {}}
-                onRemove={() => {}}
-            />
-            <div className="bg-white rounded-lg border">
-                <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
-                    <div className="flex items-center gap-x-3">
-                        <GeneratedAvatar 
-                            variant="botttsNeutral"
-                            seed={data?.name} 
-                            className="size-10"
-                        />
-                        <h2 className="text-2xl font-medium">{data.name}</h2>
-                    </div>
-                    <Badge
-                    variant="outline"
-                    className="flex items-center gap-x-2 [&>svg]:size-4"
-                    >
-                        <VideoIcon className="text-blue-700" />
-                        {data.meetingCount} {data.meetingCount === 1 ? "meeting" : "meetings"}
-                    </Badge>
-                    <div className="flex flex-col gap-y-4">
-                        <p className="font-medium text-lg">Instructions</p>
-                        <p className="text-neutral-800">{data.instructions}</p>
+    const router = useRouter();
+    const queryClient = useQueryClient();
 
+    const [updateAgentDialogOpen, setUpdateAgentDialogOpen] = useState(false);
+
+    const { data } = useSuspenseQuery(trpc.agents.getOne.queryOptions({ id: agentId }));
+
+    const removeAgent = useMutation(
+        trpc.agents.remove.mutationOptions({
+            onSuccess: async () => {
+                await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}));
+                //todo: invalidate free tier usage
+                router.push("/agents");
+            },
+            onError: (error) => {
+                toast.error(error.message);
+            },
+        }),
+    );
+
+    const [RemoveConfirmation, confirmRemove] = useConfirm("Are you sure?",
+        `This following action will remove ${data?.meetingCount} associated meetings`,
+    );
+
+    const handleRemoveAgent = async () => {
+        const ok = await confirmRemove();
+
+        if (!ok) return;
+
+        await removeAgent.mutate({ id: agentId });
+    }
+        
+
+    return (
+        <>
+            <RemoveConfirmation />
+            <UpdateAgentDialog
+                open={updateAgentDialogOpen}
+                onOpenChange={setUpdateAgentDialogOpen}
+                initialValues={data}
+            />
+            <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+                <AgentIdViewHeader
+                    agentId={agentId}
+                    agentName={data?.name}
+                    onEdit={() => setUpdateAgentDialogOpen(true)}
+                    onRemove={handleRemoveAgent}
+                />
+                <div className="bg-white rounded-lg border">
+                    <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+                        <div className="flex items-center gap-x-3">
+                            <GeneratedAvatar 
+                                variant="botttsNeutral"
+                                seed={data?.name} 
+                                className="size-10"
+                            />
+                            <h2 className="text-2xl font-medium">{data.name}</h2>
+                        </div>
+                        <Badge
+                        variant="outline"
+                        className="flex items-center gap-x-2 [&>svg]:size-4"
+                        >
+                            <VideoIcon className="text-blue-700" />
+                            {data.meetingCount} {data.meetingCount === 1 ? "meeting" : "meetings"}
+                        </Badge>
+                        <div className="flex flex-col gap-y-4">
+                            <p className="font-medium text-lg">Instructions</p>
+                            <p className="text-neutral-800">{data.instructions}</p>
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
